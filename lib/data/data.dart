6f36e7b14a6123e40b543b72bf4f41c38e5bc8a2
YongsakSaengsuk <Yongsaksaengsuk@gmail.com>
2025-02-01 19:50:32 +0700

    listcard

---
+++ lib/data/data.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_todolist/model.dart';
+
+List<Model> todos = [
+  Model(title: "Buy Milk", isDone: false),
+  Model(title: "Cleaning", isDone: false),
+  Model(title: "Study", isDone: false),
+  Model(title: "Workout", isDone: false),
+];
--- lib/todolistpage.dart
+++ lib/todolistpage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_todolist/data/data.dart';
 
 class Todolistpage extends StatefulWidget {
   const Todolistpage({super.key});
@@ -21,15 +22,25 @@ class _TodolistpageState extends State<Todolistpage> {
         padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 50),
         child: Center(
           child: Container(
-            width: 370,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(30),
-              color: Color(0xFFA6CDC6),
-            ),
-            child: ListView(
-              children: [],
-            )
-          ),
+              width: 370,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30),
+                color: Color(0xFFA6CDC6),
+              ),
+              child: ListView.builder(
+                scrollDirection: Axis.vertical,
+                itemCount: todos.length,
+                itemBuilder: (context, index) {
+                  return Card(
+                    color: Color(0xFFFBF5DD),
+                    child: ListTile(
+                      title: Text(todos[index].title),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20)),
+                    ),
+                  );
+                },
+              )),
         ),
       ),
     );
